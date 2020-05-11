71af24cc33a828978f05a27c9ca5cf52f14d29fa
Kelsey Shiba <kelsey.shiba@gmail.com>
2020-05-11 23:00:52 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -20,7 +20,7 @@ def first_word_capitalized_and_ends_with_punctuation?(text)
 end
 
 def valid_phone_number?(phone)
-    if phone.match(((\d+)))
+    if phone.match(((\d+))) #(\d{10})
       return true
     else
       false
