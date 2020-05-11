be3c8a3ae09433b8cec0945bd0bff48723e9e3a9
Kelsey Shiba <kelsey.shiba@gmail.com>
2020-05-11 22:50:32 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -20,7 +20,7 @@ def first_word_capitalized_and_ends_with_punctuation?(text)
 end
 
 def valid_phone_number?(phone)
-  phone.split.each do |number| 
+  phone.each do |number| 
     binding.pry
     if number.match(/(\d+)(\d+)(\d+)/)
       return true
