8982a9daaeb0cd65e1b6f5d290929669815b4c51
Kelsey Shiba <kelsey.shiba@gmail.com>
2020-05-11 22:15:22 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -16,7 +16,7 @@ text.scan(^[A-Z])
 end
 
 # def valid_phone_number?(phone)
-#   phone.all? {|number| number.scan()}
+#   phone.all? {|number| number.scan((\d+)}
 # end
 
 #"28894546", "(718)891-13135", "234 43 9978", "(800)IloveNY"
