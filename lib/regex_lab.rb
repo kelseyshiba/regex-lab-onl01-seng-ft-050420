82fd32ff4d90703e5dc6d2852a0df0972869d355
Kelsey Shiba <kelsey.shiba@gmail.com>
2020-05-11 22:28:03 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -12,7 +12,7 @@ def words_five_letters_long(text)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
-  if text.scan(/^[A-Z].+[\.!?]$/)
+  if text.match(/^[A-Z].+[\.!?]$/)
     return true 
   else
     false
